b29fe6c689c520ed9c988b1cf4866f77e4279c5c
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2023-05-21 10:43:57 +0000

    Deploying to gh-pages from @ InnaTereschenko/goit-js-hw-09@120a4062c3f9db016bea059ddada61308f6c904b 🚀

---
+++ 01-color-switcher.cd4cc0e7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let a=null;t.classList.add("start-btn"),e.classList.add("stop-btn"),t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.cd4cc0e7.js.map
